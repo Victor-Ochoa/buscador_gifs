4e946acdf4ce53151ef0e7fa61f6610b621f8484
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2019-06-19 16:27:27 -0300

    Iniciando o layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:buscador_gifs/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,33 +9,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        hintColor: Colors.white
       ),
-      home: Home(),
+      home: HomePage(),
     );
   }
 }
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Buscador de gifs"),
-      ),
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(children: <Widget>[
-
-          ],),
-        ),
-      )
-    );
-  }
-}
-
