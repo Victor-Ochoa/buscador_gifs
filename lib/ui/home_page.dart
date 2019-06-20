59a36359b5df687c05b3c621b53e95d296e7b3dd
Victor Ochoa Da Silva <victor.ochoa20054@hotmail.com>
2019-06-20 17:42:59 -0300

    Criando um Indicador de Progresso

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -8,45 +9,79 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset = 0;
 
-String _search;
-int _offset = 0;
+  Future<Map> _getGifs() async {
+    http.Response response;
+    if (_search == null || _search.isEmpty)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=RqYAY2JMuii9ZAhPIgynweYX9gRc3X2r&limit=20&rating=G");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=RqYAY2JMuii9ZAhPIgynweYX9gRc3X2r&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
 
-Future<Map> _getGifs()async{
-  http.Response response;
-  if(_search == null || _search.isEmpty)
-    response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=RqYAY2JMuii9ZAhPIgynweYX9gRc3X2r&limit=20&rating=G");
-  else
-    response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=RqYAY2JMuii9ZAhPIgynweYX9gRc3X2r&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
-
-  return json.decode(response.body);
-}
+    return json.decode(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
         centerTitle: true,
       ),
       backgroundColor: Colors.black,
-      body: SingleChildScrollView(
-          child: Column(children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                decoration: InputDecoration(
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
                   labelText: "Pesquise Aqui!",
-                  labelStyle: TextStyle(color: Colors.white,),
-                  border: OutlineInputBorder()
-                ),
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18),
               textAlign: TextAlign.center,
-              ),),
-          ],),
-        ),
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Container(
+                      width: 200,
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError)
+                      return Container();
+                    else
+                      return _createGifsTable(context, snapshot);
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _createGifsTable(BuildContext context, AsyncSnapshot snapshot) {
+    return Container(
+      color: Colors.green,
     );
   }
 }
-
